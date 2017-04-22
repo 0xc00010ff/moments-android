720cfdaadd6423037848d9056a0a656be3403276
GlowingEgg <qhinman@gmail.com>
2017-04-21 21:41:58 -0700

    Adds the menu to the MakeAMomentActivity.

--- app/src/main/java/com/tikkunolam/momentsintime/MakeAMomentActivity.java
+++ app/src/main/java/com/tikkunolam/momentsintime/MakeAMomentActivity.java
@@ -2,15 +2,35 @@ package com.tikkunolam.momentsintime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MakeAMomentActivity extends AppCompatActivity {
 
     // tag for logging purposes
     final String TAG = "MakeAMomentActivity";
 
+    // ui references
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_a_moment);
+
+        // get the toolbar and set it
+        mToolbar = (Toolbar) findViewById(R.id.make_a_moment_toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // inflates the menu and applies it to the toolbar
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.make_a_moment_menu, menu);
+
+        return true;
     }
 }
